3279b381681874066923025a2122993fa2140639
Solace <chidoziesolace@gmail.com>
2025-11-05 13:56:17 +0100

    Development: Added Footer

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Create from '../components/rich-text-editor'
-import Header from '../components/Header'
+import React from "react";
+import Create from "../components/rich-text-editor";
+import Header from "../components/Header";
 
 export default function CreatePost() {
   return (
-  <>
-  <Header/>
-    <div className='max-w-3xl mx-auto py-12 '>
-        <Create/>
+    <div>
+      <Header />
+      <div className="max-w-3xl mx-auto py-12 ">
+        <Create />
+      </div>
+      {/* Space */}
     </div>
-  </>
-  )
+  );
 }
